1a0760b03da0f995268be61b5615542f262058d3
Miguel Zac <miguel.qzac@gmail.com>
2021-06-05 22:22:06 -0500

    33. Route Resource. Route::resource

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,19 @@ use App\Http\Controllers\MessageController;
 
 Route::view('/','home' )->name('home');
 Route::view('/quienes-somos','about' )->name('about');
-//Route::get('/portafolio',PortafolioController::class)->name('portafolio');
+
+Route::resource('portafolio', ProjectController::class)
+->parameters(['portafolio' => 'project'])
+->names('projects');
+/*
 Route::get('/portafolio',[ProjectController::class, 'index'])->name('projects.index');
 Route::get('/portafolio/crear',[ProjectController::class, 'create'])->name('projects.create');
 Route::get('/portafolio/{project}/editar',[ProjectController::class, 'edit'])->name('projects.edit');
 Route::patch('/portafolio/{project}',[ProjectController::class, 'update'])->name('projects.update');
-
 Route::post('/portafolio',[ProjectController::class, 'store'])->name('projects.store');
 Route::get('/portafolio/{project}',[ProjectController::class, 'show'])->name('projects.show');
-
 Route::delete('/portafolio/{project}',[ProjectController::class, 'destroy'])->name('projects.destroy');
+*/
 
 Route::view('/contacto','contact' )->name('contact');
 Route::post('/contact',[MessageController::class, 'store'])->name('messages.store');
